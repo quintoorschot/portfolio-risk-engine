576c3cc2fa94e3f9dff1d305c557986c5fc71408
quintoorschot <quintvanoorschot@gmail.com>
2026-07-25 13:53:18 +0200

    Removed old parametric VaR method from Portfolio class

--- src/dataclasses/Portfolio.py
+++ src/dataclasses/Portfolio.py
@@ -29,21 +29,6 @@ class Portfolio:
     def __iter__(self) -> Iterator[Position]:
         return iter(self.positions)
 
-    # WARNING: CURRENT IMPLEMENTATION ASSUMES ASSETS ARE INDEPENDENT (I.E. CORRELATION=0.0)
-    # def parametric_var(self, confidence_interval: float = 0.95) -> float:
-    #     """Takes a confidence interval between 0 and 1 and returns the daily parametic (variance-covariance) value-at-risk"""
-
-    #     if not 0 <= confidence_interval <= 1:
-    #         raise ValueError("[ERROR]: Confidence interval must be in range [0, 1]!")
-
-    #     total_value_at_risk: float = 0
-    #     for position in self.positions:
-    #         price_history: pd.DataFrame = get_price_data(self.connection, str(position.instrument_id))
-    #         returns: Returns = Returns(price_history)
-    #         total_value_at_risk += calculate_parametric_var(position.total_value, returns, confidence_interval)
-
-    #     return total_value_at_risk
-
 
     def _fetch_positions(self) -> List[Position]:
         """Fetches the position data from the database and stores it in the portfolio instance"""
