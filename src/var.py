aa685eff5575d305887c4cdcc931b75646f0ed6b
quintoorschot <quintvanoorschot@gmail.com>
2026-08-11 11:59:40 +0200

    Added auxiliary functions for parameter validation VaR calculation functions

--- src/var.py
+++ src/var.py
@@ -2,20 +2,36 @@ from scipy.stats import norm
 import pandas as pd
 import numpy as np
 
+
+def _validate_confidence_level(
+    confidence_level: float,
+) -> None:
+    if not 0 < confidence_level < 1:
+        raise ValueError(f"[ERROR]: Confidence level c ({confidence_level}) should be in a valid range (i.e. 0 < c < 1)!")
+
+def _validate_horizon_days(
+        horizon_days: int,
+) -> None:
+    if not isinstance(horizon_days, int) or horizon_days < 1:
+        raise ValueError(f"[ERROR]: Horizon days ({horizon_days}) should be a postive integer")
+
+def _validate_pnl_length(
+        daily_pnl: pd.Series,
+        horizon_days: int,
+) -> None:
+    if len(daily_pnl) < horizon_days:
+        raise ValueError(f"[ERROR]: Not enough observations to calculate historical VaR! The number of observations ({len(daily_pnl)}) should be larger than the value of horizon days ({horizon_days})!")
+
+
 def calculate_historical_var(
         daily_pnl: pd.Series,
         confidence_level: float = 0.95,
         horizon_days: int = 1,
     ) -> float:
 
-    if not 0 < confidence_level < 1:
-        raise ValueError(f"[ERROR]: Confidence level c ({confidence_level}) should be in a valid range (i.e. 0 < c < 1)!")
-
-    if not isinstance(horizon_days, int) or horizon_days < 1:
-        raise ValueError(f"[ERROR]: Horizon days ({horizon_days}) should be a postive integer")
-
-    if len(daily_pnl) < horizon_days:
-        raise ValueError(f"[ERROR]: Not enough observations to calculate historical VaR! The number of observations ({len(daily_pnl)}) should be larger than the value of horizon days ({horizon_days})!")
+    _validate_confidence_level(confidence_level)
+    _validate_horizon_days(horizon_days)
+    _validate_pnl_length(daily_pnl, horizon_days)
 
     horizon_pnl: pd.Series = daily_pnl.rolling(window=horizon_days).sum().dropna()
 
@@ -31,14 +47,9 @@ def calculate_parametric_var(
         horizon_days: int = 1,
     ) -> float:
 
-    if not 0 < confidence_level < 1:
-        raise ValueError(f"[ERROR]: Confidence level c ({confidence_level}) should be in a valid range (i.e. 0 < c < 1)!")
-
-    if not isinstance(horizon_days, int) or horizon_days < 1:
-        raise ValueError(f"[ERROR]: Horizon days ({horizon_days}) should be a postive integer")
-
-    if daily_pnl.empty:
-        raise ValueError(f"[ERROR] Not enough observations to calculate parametric VaR. PnL cannot be empty for parametric VaR calculations!")
+    _validate_confidence_level(confidence_level)
+    _validate_horizon_days(horizon_days)
+    _validate_pnl_length(daily_pnl, horizon_days)
 
     daily_pnl_mean: float = daily_pnl.mean()
     daily_pnl_volatility: float = daily_pnl.std()
